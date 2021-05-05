8b99cede786baa9064b55d3e627c3e3a35c70f45
sergey <faldet11@gmail.com>
2021-05-05 13:56:49 +0500

    init

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const { readFile } = require("fs/promises");
 
+const main = async () => {
+  const response = await readFile("example.json").then((r) => JSON.parse(r));
 
+  const categories = [];
 
-console.log('hello world');
+  Object.entries(response.data.vendor.products).forEach(
+    ([product_id, category]) => {
+      categories.push({ product_id, ...category });
+    }
+  );
+  console.log(categories);
+};
+
+main();
